a4d949c3414cfa08d8743da8e94e1a9863559ad8
cheikhAhmad12 <seydinacheikh1999@gmail.com>
2025-11-20 08:46:27 +0100

    lstm

---
+++ test.py
@@ -0,0 +1,18 @@
+from dbus import UnknownMethodException
+from predictor import predict_engine
+
+print(predict_engine(1, 'test_FD001.txt'))
+deijiejde
+dijejdijd
+
+dedjiijed
+edhiijed
+jijide
+deiijijde
+dazhizd
+edjijeded
+
+eiijde
+dejijdieje ijiedijd 
+deuhuhedokokokdeoeokdod
+UnknownMethodExceptiondeuuhed
